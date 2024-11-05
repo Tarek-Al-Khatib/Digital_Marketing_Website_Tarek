b7ec04c7a986e817aad197f955409b90005767a1
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-11-05 23:16:04 +0200

    Added more animation for the footer

--- scripts/animation.js
+++ scripts/animation.js
@@ -18,4 +18,8 @@ timeline
   .from(".fifth-image", { x: "-300%" })
   .to(".fifth-image", { x: "0%" })
   .from(".metho1", { y: "1000%" })
-  .to(".metho1", { y: "0%" });
+  .to(".metho1", { y: "0%" })
+  .from(".cta-logo", { x: "-400%" })
+  .to(".cta-logo", { x: "0%" })
+  .from(".cta-text", { x: "400%" })
+  .to(".cta-text", { x: "0%" });
