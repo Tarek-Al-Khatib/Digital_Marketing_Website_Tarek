30ebd814e8a944d9625e6bb5e358b3af8984bebc
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-11-05 23:14:10 +0200

    Added some animations for the sections

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,3 +1,21 @@
-const timeline = gsap.timeline({ defaults: { duration: 1 } });
+const timeline = gsap.timeline({
+  defaults: { duration: 0.3 },
+});
 
-timeline.from(".header", { y: "-100%", ease: "bounce" });
+timeline
+  .from(".header-img", { y: "-100%" })
+  .from(".title", { x: "100%" })
+  .from(".slogan", { y: "9000%" })
+  .from(".overview", { opacity: 0, stagger: 0.5 })
+  .from(".first-image", { x: "-300%" })
+  .to(".first-image", { x: "0%" })
+  .from(".second-image", { x: "-300%" })
+  .to(".second-image", { x: "0%" })
+  .from(".third-image", { x: "-300%" })
+  .to(".third-image", { x: "0%" })
+  .from(".fourth-image", { x: "-300%" })
+  .to(".fourth-image", { x: "0%" })
+  .from(".fifth-image", { x: "-300%" })
+  .to(".fifth-image", { x: "0%" })
+  .from(".metho1", { y: "1000%" })
+  .to(".metho1", { y: "0%" });
